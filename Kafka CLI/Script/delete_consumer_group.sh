09cac08712358add300afe6902722b965a75d4be
MWN <91609578+immwn@users.noreply.github.com>
2023-10-12 23:03:28 +0700

    Script for Delete Consumer Group

--- Kafka CLI/Script/delete_consumer_group.sh
+++ Kafka CLI/Script/delete_consumer_group.sh
@@ -1,1 +1,9 @@
-asdfasdf
+#!/bin/bash
+
+for i in consumer_group_name
+
+do
+    echo $i
+    docker exec -it zookeeper kafka-consumer-groups --bootstrap-server broker:9092 --delete --group $i
+
+done
